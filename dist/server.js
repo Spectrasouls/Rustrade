83f55129350e75160e646339fd75f10fed678597
Stanislav <abcent.80@gmail.com>
2018-10-21 04:10:11 +0300

    Add files via upload

--- dist/server.js
+++ dist/server.js
@@ -1,15 +1,14 @@
-var express = require('express');
-var secure = require('express-force-https');
-var serveStatic = require('serve-static');
-var path = require('path');
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
 
 
-var app = express();
+const app = express();
 
 
-app.use("/", serveStatic (path.join (__dirname, '/dist')));
+app.use('/', serveStatic(path.join (__dirname, '/dist')));
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8080;
 app.listen(port);
 
 console.log('Server started on port' + port);
